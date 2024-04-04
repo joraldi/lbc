e4f12a7ec719cba099a4f74a54e5963beb2b368f
joraldi <joralyricci@gmail.com>
2024-04-04 13:41:50 +0200

    first commit

---
+++ site/View/include/connexion_bdd.php
@@ -0,0 +1,11 @@
+<?php
+
+//$id= mysqli_connect("127.0.0.1","root","","lbc");
+$pdo = new PDO('mysql:host=localhost;port=3308;dbname=lbc;charset=utf8', 'root', '');
+//Julie:
+//$pdo = new PDO('mysql:host=localhost;dbname=lbc;charset=utf8', 'root', '');
+//$id= mysqli_connect("127.0.0.1:3307","root","","lbc");
+
+
+//$pdo = new PDO('mysql:host=localhost:8889;dbname=lbc;charset=utf8', 'root', 'root');
+?>
