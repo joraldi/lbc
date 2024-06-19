cc6fbbb12635c373f9a0e9302a722bf3a59533b2
MeurphyBIAHOUILA <biahouilameurphy@gmail.com>
2024-06-19 23:47:19 +0200

    Résolution des conflits dans connexion_bdd.php en gardant mysqli_connect

--- site/View/include/connexion_bdd.php
+++ site/View/include/connexion_bdd.php
@@ -1,22 +1,3 @@
 <?php
-<<<<<<< HEAD
-//Léo Astrid:
-//$id= mysqli_connect("127.0.0.1","root","","lbc");
-$pdo = new PDO('mysql:host=localhost;dbname=lbc;charset=utf8', 'root', '');
-//Julie:
-//$pdo = new PDO('mysql:host=localhost:3307;dbname=lbc;charset=utf8', 'root', '');
-//$id= mysqli_connect("127.0.0.1:3307","root","","lbc");
-
-//Emma Lou:
-=======
-
-//$id= mysqli_connect("127.0.0.1","root","","lbc");
-$pdo = new PDO('mysql:host=localhost;port=3308;dbname=lbc;charset=utf8', 'root', '');
-//Julie:
-//$pdo = new PDO('mysql:host=localhost;dbname=lbc;charset=utf8', 'root', '');
-//$id= mysqli_connect("127.0.0.1:3307","root","","lbc");
-
-
->>>>>>> e4f12a7ec719cba099a4f74a54e5963beb2b368f
-//$pdo = new PDO('mysql:host=localhost:8889;dbname=lbc;charset=utf8', 'root', 'root');
+$id = mysqli_connect("127.0.0.1", "root", "", "lbc");
 ?>
